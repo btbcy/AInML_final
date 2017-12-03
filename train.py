fc84e0686683a6243e8d7c508372ce4fd62a43c4
BC Yang <nieil9540@gmail.com>
2017-12-03 10:43:39 +0800

    Public Simple Baseline

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 from sklearn import ensemble
 from sklearn import linear_model
+from sklearn import model_selection
 from sklearn import cross_validation, metrics
 from sklearn.grid_search import GridSearchCV
 from sklearn.externals import joblib
@@ -10,39 +11,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 import util
+import warnings
+warnings.filterwarnings('ignore')
+
+def get_oof(clf, x_train, y_train, x_test):
+    kf = model_selection.KFold(n_splits=5, shuffle=True, random_state=10)
+    train_iter = np.zeros((x_train.shape[0], )) # 1 * 15000
+    test_iter = np.zeros((x_test.shape[0], ))   # 1 * 5000
+    test_skf = np.empty((5, x_test.shape[0]))   # 5 * 5000
+
+    for idx, (train_idx, test_idx) in enumerate(kf.split(x_train)):
+        kf_x_train = x_train[train_idx] # 12000 * feature
+        kf_y_train = y_train[train_idx] # 12000 * 1
+        kf_x_test = x_train[test_idx]   # 3000 * feature
+
+        clf.fit(kf_x_train, kf_y_train)
+        train_iter[test_idx] = clf.predict_proba(kf_x_test)[:, 1] # 1* 3000
+        test_skf[idx, :] = clf.predict_proba(x_test)[:, 1] # 1 * 5000
+
+    test_iter[:] = test_skf.mean(axis=0)   # 1 * 5000
+    train_iter = train_iter.reshape(-1, 1) # 15000 * 1
+    test_iter = test_iter.reshape(-1, 1)   # 5000 * 1
+    return train_iter, test_iter
 
 trainData = pd.read_csv(sys.argv[1])
-# print trainData.info()
-# print trainData.describe()
 
-# fig = plt.figure()
-# fig.set(alpha = 0.2)
-
-# plt.subplot2grid((1, 4), (0, 0))
-# plt.scatter(trainData.Y, trainData.AGE)
-# plt.ylabel(u"AGE")
-# plt.grid(b = True, which = 'major', axis = 'y')
-# plt.subplot2grid((1, 4), (0, 1))
-# trainData.Y.value_counts().plot(kind = 'bar')
-# plt.title('Y')
-# plt.subplot2grid((1, 4), (0, 2), colspan = 2)
-# trainData.AGE[trainData.EDUCATION == 3].plot(kind='kde')
-# trainData.AGE[trainData.EDUCATION == 4].plot(kind='kde')
-# trainData.AGE[trainData.EDUCATION == 5].plot(kind='kde')
-# trainData.AGE[trainData.EDUCATION == 6].plot(kind='kde')
-# plt.xlabel(u"AGE")
-# plt.legend((u'edu3', u'edu4', u'edu5', u'edu6'), loc='best')
-# plt.show()
-
-# Y_0 = trainData.MARRIAGE[trainData.Y == 0].value_counts()
-# Y_1 = trainData.MARRIAGE[trainData.Y == 1].value_counts()
-# df = pd.DataFrame({u'Y=1':Y_1, u'Y=0':Y_0})
-# df.plot(kind = 'bar', stacked=True)
-# plt.show()
-
-# trainData = trainData.as_matrix()
-# trainX = trainData[:, 1:-2]
-# trainY = trainData[:, -1]
 trainX, trainY = util.transfer(trainData, hasY=True)
 
 # param_test1 = {'max_depth':range(3, 14, 2), 'min_samples_split':range(50, 201, 20)}
@@ -52,33 +45,55 @@ trainX, trainY = util.transfer(trainData, hasY=True)
 # print gsearch1.best_params_, gsearch1.best_score_
 
 # model = ensemble.RandomForestClassifier(criterion = 'entropy', n_estimators = 130, min_samples_split = 130, max_depth = 13, max_features = 'auto', oob_score = True, random_state = 10)
-# model = ensemble.GradientBoostingClassifier(n_estimators = 100, random_state = 10)
-# model = ensemble.ExtraTreesClassifier(criterion = 'entropy', n_estimators = 130, bootstrap=True, oob_score = True, random_state = 10)
+# gbc = ensemble.GradientBoostingClassifier(n_estimators = 100, random_state = 10)
+# etc = ensemble.ExtraTreesClassifier(criterion = 'entropy', n_estimators = 130, bootstrap=True, oob_score = True, random_state = 10)
 model = xgb.XGBClassifier(n_estimators = 150)
-# model = ensemble.AdaBoostClassifier(n_estimators = 50, random_state = 10)
+# adc = ensemble.AdaBoostClassifier(n_estimators = 50, random_state = 10)
 model.fit(trainX, trainY)
 # joblib.dump(model, sys.argv[2])
 
 #-------------------------------------------------
-testData= pd.read_csv("toTest.csv")
-testX, testY = util.transfer(testData, hasY=True)
+# testData = pd.read_csv("toTest.csv")
+# testX, testY = util.transfer(testData, hasY=True)
+# testX, testY = util.transfer(trainData[:5000], hasY=True)
 
-# numRounds = 300
-# params = {'booster':'gbtree'}
-# xgbtrain = xgb.DMatrix(trainX, trainY)
-# xgbtest = xgb.DMatrix(testX, testY)
-# watchlist = [(xgbtrain, 'train'), (xgbtest, 'val')]
-# model = xgb.train(params, dtrain=xgbtrain, num_boost_round=numRounds, evals=watchlist, early_stopping_rounds = 100)
-# print model.get_score()
+testData = pd.read_csv("Test_Public.csv")
+testX, testY = util.transfer(testData, hasY=False)
 
-print model.feature_importances_
-print "score\t", model.score(testX, testY)
+# rf_oof_train, rf_oof_test = get_oof(rfc, trainX, trainY, testX)
+# gb_oof_train, gb_oof_test = get_oof(gbc, trainX, trainY, testX)
+# et_oof_train, et_oof_test = get_oof(etc, trainX, trainY, testX)
+# xg_oof_train, xg_oof_test = get_oof(xgc, trainX, trainY, testX)
+# ad_oof_train, ad_oof_test = get_oof(adc, trainX, trainY, testX)
+# base_predictions_train = pd.DataFrame({
+#     'RandomForest': rf_oof_train.ravel(),
+#     'GradientBoost': gb_oof_train.ravel(),
+#     'ExtraTrees': et_oof_train.ravel(),
+#     'XGBoost': xg_oof_train.ravel(),
+#     'AdaBoost': ad_oof_train.ravel()
+# })
+# trainX_2nd = np.concatenate((rf_oof_train, gb_oof_train, et_oof_train, xg_oof_train, ad_oof_train), axis=1)
+# testX_2nd = np.concatenate((rf_oof_test, gb_oof_test, et_oof_test, xg_oof_test, ad_oof_test), axis=1)
+# model = xgb.XGBClassifier(n_estimators = 100)
+# model.fit(trainX, trainY)
+
+# print model.feature_importances_
+# print "score\t", model.score(testX, testY)
 test_prob = model.predict_proba(testX)
-print "auc\t", metrics.roc_auc_score(testY, test_prob[:, 1])
+test_pre = model.predict(testX)
+# print "auc\t", metrics.roc_auc_score(testY, test_prob[:, 1])
 
 rankTest = []
 for index in range(len(testX)):
-    rankTest.append([index + 1, test_prob[index][1], testY[index]])
+    if testY is not None:
+        rankTest.append([index + 1, test_prob[index][1], testY[index]])
+    else:
+        rankTest.append([index + 1, test_prob[index][1], test_pre[index]])
 rankTest = sorted(rankTest, key = lambda x : x[1])
 rankTest.reverse()
-print "kaggle\t", util.avpre(rankTest, 500)
+# print "kaggle\t", util.avpre(rankTest, 500)
+rankTest = np.asarray(rankTest)
+public_test = pd.DataFrame({'Rank_ID': rankTest[:, 0]})
+public_test.Rank_ID = public_test.Rank_ID.astype('int')
+public_test.to_csv("public3.csv", index=False)
+
--- util.py
+++ util.py
@@ -28,20 +28,17 @@ def discre(data, index,  maximum, minimum, number):
     return data
 
 def transfer(data, hasY = False):
-    dataY = []
+    dataY = None
     if hasY:
         dataY = data['Y']
         data = data.drop(['Y'], axis=1)
-    data['BILLaddPAY_1'] = data['BILL_AMT1'] + data['PAY_AMT1']
-    data['BILLaddPAY_2'] = data['BILL_AMT2'] + data['PAY_AMT2']
-    data['BILLaddPAY_3'] = data['BILL_AMT3'] + data['PAY_AMT3']
-    data['BILLaddPAY_4'] = data['BILL_AMT4'] + data['PAY_AMT4']
-    data['BILLaddPAY_5'] = data['BILL_AMT5'] + data['PAY_AMT5']
-    data['BILLaddPAY_6'] = data['BILL_AMT6'] + data['PAY_AMT6']
     data['MARcorssAGE'] = data['MARRIAGE'] * data['AGE']
-    data = data.drop(['Train_ID'], axis=1)
+    data['USE'] = data['LIMIT_BAL'] - data['BILL_AMT1'] - data['PAY_AMT1']
+    data['LIMIT_AGE'] = data['LIMIT_BAL'] / data['AGE']
+
     data = data.drop(['SEX'], axis=1)
-    # data = data.drop(['MARRIAGE'], axis=1)
-    # data['PAYadd'] = data['PAY_1'] + data['PAY_2'] * 0.8 + data['PAY_3'] * 0.6 + data['PAY_4'] * 0.4 + data['PAY_5'] * 0.2 + data['PAY_6'] * 0.1
+    data = data.drop(['MARRIAGE'], axis=1)
+    # print data.describe(include='all')
     dataX = data.as_matrix()
+    dataX = dataX[:, 1:]
     return dataX, dataY
