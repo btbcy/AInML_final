9cde6d2c5b15009c1d904833823aa36fc4b697e3
BC Yang <nieil9540@gmail.com>
2017-12-16 19:05:52 +0800

    remove something in predict.py

--- predict.py
+++ predict.py
@@ -7,54 +7,29 @@ import util
 
 model = joblib.load(sys.argv[1])
 
-isTestPublic = True
+testData = pd.read_csv(sys.argv[2])
+testX, testY = util.transfer(testData, hasY=False)
 
-if not isTestPublic:
+test_prob = model.predict_proba(testX)
+rankTest = []
+for index in range(len(testX)):
+    rankTest.append([index + 1, test_prob[index][1]])
+rankTest = sorted(rankTest, key = lambda x : -x[1])
+rankTest = np.asarray(rankTest)
+public_test = pd.DataFrame({'Rank_ID': rankTest[:, 0]})
+public_test.Rank_ID = public_test.Rank_ID.astype('int')
+public_test.to_csv('public1.csv', index=False)
 
-    testData = pd.read_csv("Test_PublicA.csv")
-    testX, testY = util.transfer(testData, hasY=True)
+#--------------------------
+testData = pd.read_csv(sys.argv[3])
+testX, testY = util.transfer(testData, hasY=False)
 
-    test_prob = model.predict_proba(testX)
-    rankTest = []
-    for index in range(len(testX)):
-        rankTest.append([test_prob[index][1], testY[index]])
-    rankTest = sorted(rankTest, key = lambda x : -x[0])
-    print "kaggle ", util.avpre(rankTest, 500)
-
-    #--------------------------
-    testData = pd.read_csv("Test_PrivateA.csv")
-    testX, testY = util.transfer(testData, hasY=True)
-    test_prob = model.predict_proba(testX)
-    rankTest = []
-    for index in range(len(testX)):
-        rankTest.append([test_prob[index][1], testY[index]])
-    rankTest = sorted(rankTest, key = lambda x : -x[0])
-    print "kaggle ", util.avpre(rankTest, 500)
-
-else:
-    testData = pd.read_csv(sys.argv[2])
-    testX, testY = util.transfer(testData, hasY=False)
-
-    test_prob = model.predict_proba(testX)
-    rankTest = []
-    for index in range(len(testX)):
-        rankTest.append([index + 1, test_prob[index][1]])
-    rankTest = sorted(rankTest, key = lambda x : -x[1])
-    rankTest = np.asarray(rankTest)
-    public_test = pd.DataFrame({'Rank_ID': rankTest[:, 0]})
-    public_test.Rank_ID = public_test.Rank_ID.astype('int')
-    public_test.to_csv('public1.csv', index=False)
-
-    #--------------------------
-    testData = pd.read_csv(sys.argv[3])
-    testX, testY = util.transfer(testData, hasY=False)
-
-    test_prob = model.predict_proba(testX)
-    rankTest = []
-    for index in range(len(testX)):
-        rankTest.append([index + 5001, test_prob[index][1]])
-    rankTest = sorted(rankTest, key = lambda x : -x[1])
-    rankTest = np.asarray(rankTest)
-    public_test = pd.DataFrame({'Rank_ID': rankTest[:, 0]})
-    public_test.Rank_ID = public_test.Rank_ID.astype('int')
-    public_test.to_csv('private.csv', index=False)
+test_prob = model.predict_proba(testX)
+rankTest = []
+for index in range(len(testX)):
+    rankTest.append([index + 5001, test_prob[index][1]])
+rankTest = sorted(rankTest, key = lambda x : -x[1])
+rankTest = np.asarray(rankTest)
+public_test = pd.DataFrame({'Rank_ID': rankTest[:, 0]})
+public_test.Rank_ID = public_test.Rank_ID.astype('int')
+public_test.to_csv('private.csv', index=False)
