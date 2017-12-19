a3c57254be7cce8e2fc487f24f0ce6f4f923421d
BC Yang <nieil9540@gmail.com>
2017-12-19 23:02:27 +0800

    0.80072

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 from sklearn import ensemble
 from sklearn.externals import joblib
 import xgboost as xgb
+import lightgbm as lgb
 import pandas as pd
 from pandas import Series, DataFrame
 import numpy as np
@@ -12,10 +13,11 @@ dataX, dataY= util.transfer(trainData, hasY=True)
 
 model = list()
 # model.append(ensemble.RandomForestClassifier(criterion='entropy', n_estimators=130, max_depth=12, oob_score=True, random_state=10))
-model.append(ensemble.ExtraTreesClassifier(criterion='entropy', n_estimators=100, max_depth=16, bootstrap=True, oob_score=True, random_state=10))
+model.append(ensemble.ExtraTreesClassifier(criterion='entropy', n_estimators=98, max_depth=16, bootstrap=True, oob_score=True, random_state=10))
 model.append(ensemble.GradientBoostingClassifier(n_estimators=100, max_depth=4, random_state=10))
-model.append(xgb.XGBClassifier(n_estimators=160, max_depth=4))
-model.append(ensemble.VotingClassifier(estimators=[('et', model[0]), ('gdb', model[1]), ('xgb', model[2])], voting='soft', weights=[1, 1, 1]))
+model.append(xgb.XGBClassifier(n_estimators=158, max_depth=4))
+model.append(lgb.LGBMClassifier(n_estimators=125, max_depth=5, num_leaves=100))
+model.append(ensemble.VotingClassifier(estimators=[('et', model[0]), ('gdb', model[1]), ('xgb', model[2]), ('lgb', model[3])], voting='soft', weights=[1, 1, 1, 1]))
 
 for i in range(len(model)):
     model[i].fit(dataX, dataY)
