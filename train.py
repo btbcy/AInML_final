f5b9ecc399d748b2b745c3703cc771a54ef96f98
BC Yang <nieil9540@gmail.com>
2017-12-20 17:31:19 +0800

    nothing

--- train.py
+++ train.py
@@ -44,9 +44,11 @@ model.append(ensemble.ExtraTreesClassifier(criterion='entropy', n_estimators=98,
 model.append(ensemble.GradientBoostingClassifier(n_estimators=126, max_depth=4, max_leaf_nodes=30, random_state=10))
 model.append(xgb.XGBClassifier(n_estimators=158, max_depth=4))
 model.append(lgb.LGBMClassifier(n_estimators=125, max_depth=5, num_leaves=100))
+
 # model.append(ensemble.RandomForestClassifier(criterion='entropy', n_estimators=135, max_depth=12, oob_score=True, random_state=10))
 # model.append(ensemble.GradientBoostingClassifier(n_estimators=100, max_depth=4, random_state=10))
 # model.append(ensemble.VotingClassifier(estimators=[('et', model[0]), ('gdb', model[1]), ('xgb', model[2])], voting='soft', weights=[1, 1, 1.4]))
+
 model.append(ensemble.VotingClassifier(estimators=[('et', model[0]), ('gdb', model[1]), ('xgb', model[2]), ('lgb', model[3])], voting='soft', weights=[1, 1, 1, 1]))
 # joblib.dump(model, sys.argv[2])
 
